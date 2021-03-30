86b7598db68900df107f394dcc6ce0de2a2b948d
venom <venom@withufuture.com>
2021-03-30 11:29:49 +0800

    code

---
+++ hello-netty-client/src/main/java/com/shanks/client/ClientHandlersInitializer.java
@@ -0,0 +1,22 @@
+package com.shanks.client;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+
+/**
+ * FileName    : com.shanks.client
+ * Description :
+ *
+ * @author : 毒液
+ * @version : 1.0
+ * Create Date : 2021/3/30 9:59
+ * @Copyright : WithUFuture Software Co.,Ltd.Rights Reserved
+ * @Company : 深圳幻影未来信息科技有限公司
+ **/
+public class ClientHandlersInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel socketChannel) throws Exception {
+
+    }
+}
